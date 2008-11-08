0b9c4d9342655abd843e765ebebbe0e6d62b4c9e
Jed Brown <jed@59A2.org>
2008-11-08 13:10:48 +0100

    Change my mind again, use XXX_INCLUDES like FindQt4.cmake
    Signed-off-by: Jed Brown <jed@59A2.org>

--- FindFFTW.cmake
+++ FindFFTW.cmake
@@ -1,22 +1,22 @@
 # - Find FFTW
 # Find the native FFTW includes and library
 #
-#  FFTW_INCLUDE_DIRS - where to find gsl/gsl_*.h, etc.
+#  FFTW_INCLUDES    - where to find fftw3.h
 #  FFTW_LIBRARIES   - List of libraries when using FFTW.
 #  FFTW_FOUND       - True if FFTW found.
 
-if (FFTW_INCLUDE_DIRS)
+if (FFTW_INCLUDES)
   # Already in cache, be silent
   set (FFTW_FIND_QUIETLY TRUE)
-endif (FFTW_INCLUDE_DIRS)
+endif (FFTW_INCLUDES)
 
-find_path (FFTW_INCLUDE_DIRS fftw3.h)
+find_path (FFTW_INCLUDES fftw3.h)
 
 find_library (FFTW_LIBRARIES NAMES fftw3)
 
 # handle the QUIETLY and REQUIRED arguments and set FFTW_FOUND to TRUE if 
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDE_DIRS)
+find_package_handle_standard_args (FFTW DEFAULT_MSG FFTW_LIBRARIES FFTW_INCLUDES)
 
-mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDE_DIRS)
+mark_as_advanced (FFTW_LIBRARIES FFTW_INCLUDES)
--- FindGSL.cmake
+++ FindGSL.cmake
@@ -1,23 +1,23 @@
 # - Find GSL
 # Find the native GSL includes and library
 #
-#  GSL_INCLUDE_DIRS - where to find gsl/gsl_*.h, etc.
+#  GSL_INCLUDES    - where to find gsl/gsl_*.h, etc.
 #  GSL_LIBRARIES   - List of libraries when using GSL.
 #  GSL_FOUND       - True if GSL found.
 
 
-if (GSL_INCLUDE_DIRS)
+if (GSL_INCLUDES)
   # Already in cache, be silent
   set (GSL_FIND_QUIETLY TRUE)
-endif (GSL_INCLUDE_DIRS)
+endif (GSL_INCLUDES)
 
-find_path (GSL_INCLUDE_DIRS gsl/gsl_math.h)
+find_path (GSL_INCLUDES gsl/gsl_math.h)
 
 find_library (GSL_LIBRARIES NAMES gsl)
 
 # handle the QUIETLY and REQUIRED arguments and set GSL_FOUND to TRUE if 
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (GSL DEFAULT_MSG GSL_LIBRARIES GSL_INCLUDE_DIRS)
+find_package_handle_standard_args (GSL DEFAULT_MSG GSL_LIBRARIES GSL_INCLUDES)
 
-mark_as_advanced (GSL_LIBRARIES GSL_INCLUDE_DIRS)
+mark_as_advanced (GSL_LIBRARIES GSL_INCLUDES)
