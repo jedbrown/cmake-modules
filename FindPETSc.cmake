26f8545c7347da36a243bde5ec3794fff7e329ea
Jacob Merson <jacob.merson@gmail.com>
2015-12-29 02:39:39 -0500

    Added external libraries as output variable
    In oder to link to PETSc with a c++ project, the compiler must have
    access to the mpi library files. Another method which is not preferred
    in CMAKE is to set the compiler to mpicc which is packaged as part of
    PETSc.

--- FindPETSc.cmake
+++ FindPETSc.cmake
@@ -4,6 +4,7 @@
 #  PETSC_FOUND        - system has PETSc
 #  PETSC_INCLUDES     - the PETSc include directories
 #  PETSC_LIBRARIES    - Link these to use PETSc
+#  PETSC_EXTERNAL_LIBRARIES    - Link these to get external libraries e.g. mpi from PETSc
 #  PETSC_COMPILER     - Compiler used by PETSc, helpful to find a compatible MPI
 #  PETSC_DEFINITIONS  - Compiler switches for using PETSc
 #  PETSC_MPIEXEC      - Executable for running MPI programs
@@ -316,11 +317,12 @@ int main(int argc,char *argv[]) {
   set (PETSC_MPIEXEC ${petsc_mpiexec} CACHE FILEPATH "Executable for running PETSc MPI programs" FORCE)
   set (PETSC_INCLUDES ${petsc_includes_needed} CACHE STRING "PETSc include path" FORCE)
   set (PETSC_LIBRARIES ${PETSC_LIBRARIES_ALL} CACHE STRING "PETSc libraries" FORCE)
+  set (PETSC_EXTERNAL_LIBRARIES ${petsc_libs_external} CACHE STRING "PETSc libraries" FORCE)
   set (PETSC_COMPILER ${petsc_cc} CACHE FILEPATH "PETSc compiler" FORCE)
   # Note that we have forced values for all these choices.  If you
   # change these, you are telling the system to trust you that they
   # work.  It is likely that you will end up with a broken build.
-  mark_as_advanced (PETSC_INCLUDES PETSC_LIBRARIES PETSC_COMPILER PETSC_DEFINITIONS PETSC_MPIEXEC PETSC_EXECUTABLE_RUNS)
+  mark_as_advanced (PETSC_INCLUDES PETSC_LIBRARIES PETSC_EXTERNAL_LIBRARIES PETSC_COMPILER PETSC_DEFINITIONS PETSC_MPIEXEC PETSC_EXECUTABLE_RUNS)
 endif ()
 
 include (FindPackageHandleStandardArgs)
